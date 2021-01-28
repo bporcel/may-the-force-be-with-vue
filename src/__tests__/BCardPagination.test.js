77659012eb58cde0193b038feab7c85a85b9fa41
bporcel <codamming@gmail.com>
2021-01-28 22:38:59 +0100

    Modified pagination test

--- src/__tests__/BCardPagination.test.js
+++ src/__tests__/BCardPagination.test.js
@@ -14,6 +14,7 @@ const defaultProps = {
   data: [
     { name: 'Name', url: '0/1/2/3/4/5' },
     { name: 'Name1', url: '0/1/2/3/4/51' },
+    { name: 'Name2', url: '0/1/2/3/4/52' },
   ],
   handleClickCard: () => {},
 };
@@ -33,40 +34,22 @@ describe('BCardPagination', () => {
     );
   });
 
-  it('Renders cards according to items length', () => {
+  it('Renders cards according to items length and button according to totalPages', () => {
     const wrapper = factory(defaultProps);
 
     expect(wrapper.findAllComponents(BCard)).toHaveLength(
       wrapper.vm.items.length
     );
-  });
-
-  it('Renders buttons according to totalPages value', () => {
-    const wrapper = factory(defaultProps);
-
     expect(wrapper.findAll('button')).toHaveLength(wrapper.vm.totalPages);
   });
 
-  it('Check state default values', () => {
-    const wrapper = factory(defaultProps);
-
-    expect(wrapper.vm.currentPage).toBe(1);
-    expect(wrapper.vm.items).toStrictEqual(
-      defaultProps.data.slice(0, defaultProps.perPage)
-    );
-    expect(wrapper.vm.filtered).toStrictEqual([]);
-    expect(wrapper.vm.totalPages).toBe(
-      Math.ceil(defaultProps.records / defaultProps.perPage)
-    );
-  });
-
-  it('Click on page btn should change currentPage to given id', () => {
+  it('Click on page btn should change currentPage to given id and slice data', () => {
     const wrapper = factory(defaultProps);
 
     wrapper.vm.handleClickButton('2');
 
     expect(wrapper.vm.currentPage).toBe(2);
-    expect(wrapper.vm.items.length).not.toBe(0);
+    expect(wrapper.vm.items.length).toBe(defaultProps.perPage);
   });
 
   it('Handle filter', () => {
